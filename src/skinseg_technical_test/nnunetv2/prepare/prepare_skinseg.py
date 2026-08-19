9d72e3953670478860af33face0068791fe801bc
amyn <kassara.amyn@gmail.com>
2026-08-19 09:47:32 +0000

    fix bug of missing cases

--- src/skinseg_technical_test/nnunetv2/prepare/prepare_skinseg.py
+++ src/skinseg_technical_test/nnunetv2/prepare/prepare_skinseg.py
@@ -73,10 +73,10 @@ def prepare_skinseg(dataset_id: int, task_name: str, test_size: float, n_folds: 
     out_dir, imagesTr_dir, labelsTr_dir, imagesTs_dir, preprocessed_dir = make_out_dirs(dataset_id, task_name)
     setup_logging(preprocessed_dir / "logs")
     logging.info(f"Starting dataset preparation for task '{task_name}' with dataset ID {dataset_id}.")
-    df = pd.read_csv(data_path / 'labels.csv')
-    train_val, test, patient_metadata = get_train_test_split(df, test_size, n_folds)
+    df_labels = pd.read_csv(data_path / 'labels.csv')
+    train_val, test, patient_metadata = get_train_test_split(df_labels, test_size, n_folds)
     df = pd.merge(
-    left=df, 
+    left=df_labels, 
     right=patient_metadata,
     how='left',
     left_on=['patient', 'region'],
@@ -91,59 +91,54 @@ def prepare_skinseg(dataset_id: int, task_name: str, test_size: float, n_folds: 
         'image_path': ['path/to/image1.nii', 'path/to/image2.nii'],
         'label_path': ['path/to/label1.nii', 'path/to/label2.nii']
     })
-
     # Process each image and label
     train_pids = 0
     test_pids = 0
-    for idx, row in patient_metadata.iterrows():
+    for idx, row in df.iterrows():
         patient_id = row["patient_id"]
         patient = row["patient"]
         df_img = df[df['patient_id'] == patient_id]
-        img_names = df_img['img_name'].values
-        for img_name in img_names:
-            img_path = data_path / 'imgs' / img_name
-            label_path = data_path / 'masks' / f"{img_name.split('.png')[0]}.png"     
-            if patient_id in test:
-                img_dest = imagesTs_dir / f"{img_name.split('.png')[0]}_0000.nii.gz"
-                if img_dest.exists() and not regenerate:
-                    logging.info(f"Test image {img_dest} already exists. Skipping conversion.")
-                    continue
-                img_array = load_mask_as_array(img_path, type='img')
-                img_array, _ = crop_to_common_shape(img_array, np.zeros_like(img_array))
-                # Convert to NIfTI and save in imagesTs directory
-                im_to_niftii(img_array, img_dest)
-                test_pids += 1
-                logging.info(f"Processed test image {img_name} for patient {patient_id}")   
-            else:
-                # Convert to NIfTI and save in imagesTr and labelsTr directories
-                img_dest = imagesTr_dir / f"{img_name.split('.png')[0]}_0000.nii.gz"
-                label_dest = labelsTr_dir / f"{img_name.split('.png')[0]}.nii.gz"
-                if img_dest.exists() and label_dest.exists() and not regenerate:
-                    logging.info(f"Train image {img_dest} and label {label_dest} already exist. Skipping conversion.")
-                    continue
-                img_array = load_mask_as_array(img_path, type='img')
-                label_array = load_mask_as_array(label_path, type='mask')
-                img_array, label_array = crop_to_common_shape(img_array, label_array)
-                im_to_niftii(img_array, img_dest)
-                im_to_niftii(label_array, label_dest, type='mask')
-                train_pids += 1
-                logging.info(f"Processed train image {img_name} for patient {patient_id}")    
-    # dump json and csv
+        img_name = row['img_name']
+        img_path = data_path / 'imgs' / img_name
+        label_path = data_path / 'masks' / f"{img_name.split('.png')[0]}.png"     
+        if patient_id in test:
+            img_dest = imagesTs_dir / f"{img_name.split('.png')[0]}_0000.nii.gz"
+            if img_dest.exists() and not regenerate:
+                logging.info(f"Test image {img_dest} already exists. Skipping conversion.")
+                continue
+            img_array = load_mask_as_array(img_path, type='img')
+            img_array, _ = crop_to_common_shape(img_array, np.zeros_like(img_array))
+            # Convert to NIfTI and save in imagesTs directory
+            im_to_niftii(img_array, img_dest)
+            test_pids += 1
+            logging.info(f"Processed test image {img_name} for patient {patient_id}")   
+        else:
+            # Convert to NIfTI and save in imagesTr and labelsTr directories
+            img_dest = imagesTr_dir / f"{img_name.split('.png')[0]}_0000.nii.gz"
+            label_dest = labelsTr_dir / f"{img_name.split('.png')[0]}.nii.gz"
+            if img_dest.exists() and label_dest.exists() and not regenerate:
+                logging.info(f"Train image {img_dest} and label {label_dest} already exist. Skipping conversion.")
+                continue
+            img_array = load_mask_as_array(img_path, type='img')
+            label_array = load_mask_as_array(label_path, type='mask')
+            img_array, label_array = crop_to_common_shape(img_array, label_array)
+            im_to_niftii(img_array, img_dest)
+            im_to_niftii(label_array, label_dest, type='mask')
+            train_pids += 1
+            logging.info(f"Processed train image {img_name} for patient {patient_id}")    
     #save_json(train_val, preprocessed_dir / "splits_final.json")
     patient_metadata.to_csv(preprocessed_dir / "patient_metadata.csv", index=False)
     df[df['patient_id'].isin(test)].to_csv(preprocessed_dir / "test.csv", index=False)
     # Generate dataset JSON
     label_map = {'background': 0, 'surface': 1, 'jde': 2, 'corneous': 3, "ignore": 4}
     channel_names = {0: 'lc_oct'}
-    print(train_pids)
-    print(test_pids)
+    print(f"Train images: {train_pids}, Test images: {test_pids}")
     generate_dataset_json(out_dir, channel_names=channel_names, labels=label_map,
                  num_training_cases=train_pids, file_ending=".nii.gz", 
                  dataset_name=f"Dataset{dataset_id:03d}_{task_name}")
-
     logging.info(f"Dataset preparation for task '{task_name}' completed.")
 
 
 if __name__ == "__main__":
     # Example usage
-    prepare_skinseg(dataset_id=1, task_name="SkinSegmentation", test_size= 0.25, n_folds = 3, regenerate = True)
+    prepare_skinseg(dataset_id=1, task_name="SkinSegmentation", test_size= 0.25, n_folds = 4, regenerate = True)
